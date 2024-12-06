8e5b9e2d07f54ee5d2fbeb888ef087d675a757e5
Daniel Rocha <danielrocha1507@gmail.com>
2024-12-06 03:20:06 -0300

    [+] Hospedando imagens

--- src/sections/SuportedGames/index.jsx
+++ src/sections/SuportedGames/index.jsx
@@ -7,14 +7,14 @@ const data = [
   {
     id: 1,
     type: 2,
-    image: 'https://media.discordapp.net/attachments/1313656358846988309/1313670680566304768/wallpapers-de-gta-v-em-4k-para-pc-e-celular-14-scaled.png?ex=67539da9&is=67524c29&hm=4bf10832248b90d04fc9bb5bae0093978dac454b50205a48988f688cb56f2594&=&format=webp&quality=lossless&width=1082&height=676',
+    image: '/images/fivem.png',
     title: 'FiveM',
     widthType: 'large',
   },
   {
     id: 2,
     type: 2,
-    image: 'https://media.discordapp.net/attachments/1313656358846988309/1313672198644437093/hmxiwjjqb8371.png?ex=67539f13&is=67524d93&hm=13ee7e5a62f289261ee8d8a3a6418faa0bb39e0447cdc3810f8922be0ef4f821&=&format=webp&quality=lossless&width=1202&height=676',
+    image: '/images/reddead.png',
     title: 'RedM',
     widthType: 'large',
   },
